dfd6dedab2016ce93c1885ddc07426a2acc79cdb
antocoin <info@antocoin.io>
2025-01-03 06:07:21 +0000

    validate ANTO

--- validate.sh
+++ validate.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 cue vet src/tokens/solana.tokenlist.json automerge/schema.cue -d '#Tokenlist'
+ANTO,ANTO,BvqVdf4ULyCrpMryQfBAnE8LfjvYtfUYej5WTACbRpwP,9,file:///Users/qrigal/Downloads/ANTO%20(1)%20(1).svg,true
